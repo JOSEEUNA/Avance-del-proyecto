31ac8d5cde999215f9cd42a3160c3d219059f968
JOSEEUNA <61030600+JOSEEUNA@users.noreply.github.com>
2020-03-27 18:49:37 -0600

    avance

--- Proyecto/js/paresImpares.js
+++ Proyecto/js/paresImpares.js
@@ -1,6 +1,8 @@
 const pares = () => {
+    //la variable listpar se esta igualando al ID  que se esta obteniedo
+    //  de los input en el html evaluandolo como un entero
     var listpar = document.getElementById("numeros").value;
-
+    // En esta condicion .
     if (listpar % 2 == 0) {
         return document.getElementById("numero").innerHTML = listpar + " = Es un numero Par";
     } else {
